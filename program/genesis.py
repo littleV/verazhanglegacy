e82a0ce02b8a086ae99751d57c24bd3b72dbeac1
littleV <weizhang.columbia@gmail.com>
2018-11-01 18:11:12 -0400

    three dimensional gate

--- program/genesis.py
+++ program/genesis.py
@@ -1,4 +1,5 @@
 import random
+import sys
 
 def random01():
     return random.randint(0,1)
@@ -6,32 +7,28 @@ def random01():
 # if 1+1 !=2 is false, it's easy, everything is normal
 # but if 1+1 != 2 is true, what value should it be?
 # I think it could either be more or less
-def judgeOnePlusOneEqualToTwo():
+def judgeOnePlusOne():
 	while True:
 		a = random01()
 		a = (a<<1) ^ random01()
-		if a== 0:
-			# for case 1+1 = 0
-			return 0;  
-		if a== 1:
-			# for case 1+1 = 2
-			return 1;
-		if a== 2:
-			# for case 1+1 = 4
-			return 2;
+		if a == 0:
+			return 0  
+		if a == 1:
+			return 1
+		if a == 2:
+			return 2
 
-def resultOfOnePlusOne():
-	rule = judgeOnePlusOneEqualToTwo()
-	if rule == 0:
-		return 0
-	elif rule == 1:
-		return 2
-	else:
-		return 4
-	
-# Amazingly, for all three cases above, 1 - 1 = 0 is always true 
-def resultOfOneMinusOne():
-	return 0;
+def add(a, b):
+	# convert a and b to binary numbers 
+	# adding it digit by digit using the judge rule above
+	# which is
+	#		0 case, 1 + 1 = 0
+	#		1 case, 1 + 1 = 1
+	#		2 case, 1 + 1 = 10
+	# then convert the result back to decimal numbers
+	# if unable to, throw errors
+
+
 
 
 			
